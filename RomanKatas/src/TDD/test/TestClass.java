b0d883a35aa3421c0bd315f1dc53d45e0cfb07e1
Bianca Leuca <bianca.leuca@mozaicworks.com>
2015-12-04 17:46:31 +0200

    starting TDD as if you meant it

---
+++ RomanKatas/src/TDD/test/TestClass.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by biancal on 04.12.2015.
+ */
+public class TestClass {
+
+	@Test
+	public void testToFail() {
+		assertEquals(1,3);
+	}
+}
