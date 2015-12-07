95c46e579fdd79c9db0dd3e104477d615a92b2af
Bianca Leuca <bianca.leuca@mozaicworks.com>
2015-12-07 16:36:17 +0200

    500 is D

--- RomanTransformerTDD/src/main/groovy/NumeralTransformer.groovy
+++ RomanTransformerTDD/src/main/groovy/NumeralTransformer.groovy
@@ -3,7 +3,8 @@ class NumeralTransformer {
 	                      5: new RomanNumeral(romanValue:  "V"),
 	                      10: new RomanNumeral(romanValue:  "X"),
 	                      50: new RomanNumeral(romanValue:  "L"),
-	                      100: new RomanNumeral(romanValue:  "C")]
+	                      100: new RomanNumeral(romanValue:  "C"),
+	                      500: new RomanNumeral(romanValue:  "D")]
 	def transform(int numeral) {
 		RomanNumeral romanNumeral = numeralMapping.get(numeral);
 		if(romanNumeral) {
--- RomanTransformerTDD/src/test/groovy/RomanTransformerTDDTest.groovy
+++ RomanTransformerTDD/src/test/groovy/RomanTransformerTDDTest.groovy
@@ -26,6 +26,7 @@ class RomanTransformerTDDTest extends Specification {
 		"X"        | 10
 		"L"        | 50
 		"C"        | 100
+		"D"        | 500
 
 	}
 }
