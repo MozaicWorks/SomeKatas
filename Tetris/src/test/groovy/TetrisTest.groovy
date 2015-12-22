89a80736d3faa38fe360417f4a390df4cfa9cbf7
Bianca Leuca <bianca.leuca@mozaicworks.com>
2015-12-22 16:52:58 +0200

    20 x 10 board, I piece appears on screen, assert filledBoardCells[boardLength-1][witdth/2+1] is true

--- Tetris/src/test/groovy/TetrisTest.groovy
+++ Tetris/src/test/groovy/TetrisTest.groovy
@@ -195,6 +195,18 @@ class TetrisTest extends Specification {
 		assert tetrisGame.isCellFromGivenPositionFilled(lengthPos, widthPos)
 	}
 
+	def "20 x 10 board, I piece appears on screen, assert filledBoardCells[boardLength-1][witdth/2 +1] is true"() {
+		given:
+		tetrisGame = new TetrisGame(20, 10)
+
+		when:
+		tetrisGame.initializeIPiece()
+		int lengthPos = tetrisGame.boardLength - 1
+		int widthPos = tetrisGame.boardWidth/2 + 1
+		then:
+		assert tetrisGame.isCellFromGivenPositionFilled(lengthPos, widthPos)
+	}
+
 
 
 
