99e171cdd131a6700a4574821c3be02072cc88ae
Bianca Leuca <bianca.leuca@mozaicworks.com>
2016-01-08 12:19:24 +0200

    added first todoList and task in BootStrap's init, also displayed the tasks

--- TodoListApplication/grails-app/conf/BootStrap.groovy
+++ TodoListApplication/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,13 @@
+import todolist.Task
+import todolist.TodoList
+
 class BootStrap {
 
     def init = { servletContext ->
+        def todoList = new TodoList(name: "List 1")
+        def task1 = new Task(name: "task1", assigneeName: "Dan", deadline: new Date(), done: false)
+        todoList.addToTasks(task1)
+        todoList.save(failOnError: true)
     }
     def destroy = {
     }
