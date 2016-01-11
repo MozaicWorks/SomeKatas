d4d4429116ba2990434ed342ad48499f668798d4
Bianca Leuca <bianca.leuca@mozaicworks.com>
2016-01-11 11:43:41 +0200

    added unit test

--- TodoListVs3/grails-app/conf/BootStrap.groovy
+++ TodoListVs3/grails-app/conf/BootStrap.groovy
@@ -6,15 +6,19 @@ class BootStrap {
 
     def init = { servletContext ->
 	    Task task1 = new Task(name:"task1", done: false, deadline: new Date())
+
+
 	    TodoList todoList1 = new TodoList(name: "todoList1")
 	    todoList1.addToTasks(task1)
 	    todoList1.save(failOnError: true)
 
 	    User user1 = new User(name: "user")
 	    user1.addToTasks(task1)
+
 	    user1.save(failOnError: true)
 	    task1.user = user1
 	    task1.save(failOnError: true)
+
 
 
 
--- TodoListVs3/test/unit/todolist/TodoListControllerSpec.groovy
+++ TodoListVs3/test/unit/todolist/TodoListControllerSpec.groovy
@@ -1,5 +1,6 @@
 package todolist
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
@@ -7,6 +8,7 @@ import spock.lang.Specification
  * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
  */
 @TestFor(TodoListController)
+@Mock([Task, TodoList, User])
 class TodoListControllerSpec extends Specification {
 
     def setup() {
@@ -15,6 +17,19 @@ class TodoListControllerSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "test method find by name"() {
+	    given:
+	    Task task1 = new Task(name:"task1", done: false, deadline: new Date())
+	    Task task2 = new Task(name:"task2", done: false, deadline: new Date().minus(5))
+	    TodoList todoList1 = new TodoList(name: "todoList1")
+	    todoList1.addToTasks(task1)
+	    todoList1.addToTasks(task2)
+	    todoList1.save(failOnError: true)
+
+	    when:
+	    Task result = Task.findByName("task1")
+
+	    then:
+	    assert task1, result
     }
 }
